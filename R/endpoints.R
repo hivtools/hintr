26b45f16a5b78a5c05941a20cf40a36363f19347
Robert Ashton <r.ashton@imperial.ac.uk>
2019-08-06 10:57:57 +0100

    Fix coverage

--- R/endpoints.R
+++ R/endpoints.R
@@ -1,9 +1,16 @@
-api <- function() {
+api_build <- function() {
   pr <- plumber::plumber$new()
   pr$handle("POST", "/validate", endpoint_validate_pjnz)
   pr$handle("GET", "/", api_root)
-  pr$run(host = "0.0.0.0", port = 8888)
   pr
+}
+
+api_run <- function(pr) {
+  pr$run(host = "0.0.0.0", port = 8888) # nocov
+}
+
+api <- function() {
+  api_run(api_build()) # nocov
 }
 
 endpoint_validate_pjnz <- function(req, pjnz) {
