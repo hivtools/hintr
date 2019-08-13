6f99ed58b8c4da84802fa28875d0d8792ba6a34b
Robert Ashton <r.ashton@imperial.ac.uk>
2019-08-13 16:51:36 +0100

    Add queue code and integration test

---
+++ tests/testthat/test-integration.R
@@ -0,0 +1,15 @@
+context("integration")
+
+test_that("queue can be setup and run end-to-end", {
+  local({
+    rrq <- model_queue_start(tempfile())
+    id <- model_queue_submit(list(sleep = 0.0))
+    repeat {
+      if (model_queue_status(id)$status %in% c("COMPLETE", "ERROR")) {
+        break
+      }
+    }
+    model_queue_result(id)
+  })
+
+})
