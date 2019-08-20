fb75dbfda0d3f62b5c835aa65fc67814e4cc40fd
Robert Ashton <r.ashton@imperial.ac.uk>
2019-08-19 17:59:03 -0700

    Iterate tests

--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -3,7 +3,7 @@ context("integration")
 test_that("queue can be setup and run end-to-end", {
   local({
     rrq <- model_queue_start(tempfile())
-    id <- model_queue_submit(list(sleep = 0.0))
+    id <- model_queue_submit(list(sleep = 0.0), list(no_of_iterations = 3000))
     repeat {
       if (model_queue_status(id)$status %in% c("COMPLETE", "ERROR")) {
         break
