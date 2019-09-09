834bdfeed7621d52aa3493a4f66b3b545f22000a
Robert Ashton <r.ashton@imperial.ac.uk>
2019-09-09 16:21:44 +0100

    Remove debug script

--- debug.R
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env Rscript
-library(testthat)
-source("tests/testthat/helper-server.R")
-source("tests/testthat/helper-queue.R")
-options(error = traceback)
-server <- hintr_server()
-httr::GET(server$url)
-server$process$kill()
