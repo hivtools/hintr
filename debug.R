5b077e02665d77ca97afae91e861edc8b01d0e56
Robert Ashton <r.ashton@imperial.ac.uk>
2019-09-09 15:13:17 +0100

    More verbose debug output

--- debug.R
+++ debug.R
@@ -2,6 +2,7 @@
 library(testthat)
 source("tests/testthat/helper-server.R")
 source("tests/testthat/helper-queue.R")
+options(error = traceback)
 server <- hintr_server()
 httr::GET(server$url)
 server$process$kill()
