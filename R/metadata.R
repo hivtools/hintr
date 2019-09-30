1edd9b729d0202d6de1a7312914aabf37a5caf9c
Robert Ashton <r.ashton@imperial.ac.uk>
2019-09-30 14:26:27 +0100

    Improve code quality

--- R/metadata.R
+++ R/metadata.R
@@ -1,35 +1,19 @@
 do_plotting_metadata <- function(country) {
   metadata <- naomi::get_plotting_metadata(country)
-  out <- lapply(unique(metadata$data_type), function(data_type) {
-    data <- metadata[metadata$data_type == data_type, ]
-    build_data_type_metadata(data)
-  })
-  names(out) <- unique(metadata$data_type)
-  out
+  lapply(split(metadata, metadata$data_type), build_data_type_metadata)
 }
 
 build_data_type_metadata <- function(metadata) {
-  out <- lapply(unique(metadata$plot_type), function(plot_type) {
-    data <- metadata[metadata$plot_type == plot_type, ]
-    list(
-      indicators = build_plot_type_metadata(data)
-    )
-  })
-  names(out) <- unique(metadata$plot_type)
-  out
+  lapply(split(metadata, metadata$plot_type), build_plot_type_metadata)
 }
 
 build_plot_type_metadata <- function(metadata) {
-  out <- lapply(unique(metadata$indicator), function(indicator) {
-    data <- metadata[metadata$indicator == indicator, ]
-    build_indicator_metadata(data)
-  })
-  names(out) <- unique(metadata$indicator)
-  out
+  list(indicators =
+         lapply(split(metadata, metadata$indicator), build_indicator_metadata))
 }
 
 build_indicator_metadata <- function(metadata) {
-  if(nrow(metadata) != 1) {
+  if (nrow(metadata) != 1) {
     stop("Expected only 1 row for indicator, data type, plot type combination.
 Check each combination is unique in configuration.")
   }
