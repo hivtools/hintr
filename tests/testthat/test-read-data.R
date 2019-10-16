04fa76b56f9e73d87d5a1547aec6477e0c873c00
Robert Ashton <r.ashton@imperial.ac.uk>
2019-10-16 09:55:30 +0100

    Add tests for data reading

--- tests/testthat/test-read-data.R
+++ tests/testthat/test-read-data.R
@@ -30,3 +30,35 @@ test_that("can read geojson regions", {
   expect_equal(regions[1:5], c("MWI", "MWI.1", "MWI.2", "MWI.3", "MWI.1.1"))
 })
 
+test_that("can read geojson level labels", {
+  shape <- file.path("testdata", "malawi.geojson")
+  levels <- read_level_labels(shape)
+  expect_length(levels, 5)
+  expect_equal(levels, c("Country", "Region", "Zone", "District",
+                         "District + Metro"))
+})
+
+test_that("can read regions from csv file", {
+  programme <- file.path("testdata", "programme.csv")
+  regions <- read_csv_regions(programme)
+  expect_length(regions, 32)
+  expect_equal(regions[1:3], c("MWI.3.4.18.20", "MWI.3.5.23.26",
+                               "MWI.3.5.23.29"))
+})
+
+test_that("can read iso3 from PJNZ", {
+  pjnz <- file.path("testdata", "Botswana2018.PJNZ")
+  iso3 <- read_pjnz_iso3(pjnz)
+  expect_equal(iso3, "BWA")
+})
+
+test_that("can read iso3 from geojson", {
+  shape <- file.path("testdata", "malawi.geojson")
+  iso3 <- read_geojson_iso3(shape)
+  expect_equal(iso3, "MWI")
+})
+
+test_that("can read country from PJNZ", {
+  pjnz <- file.path("testdata", "Botswana2018.PJNZ")
+  expect_equal(read_country(pjnz), "Botswana")
+})
