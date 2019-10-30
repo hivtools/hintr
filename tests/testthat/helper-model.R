df53ddf23b237fc9b2e828ef2443eb3f8dd796dc
Robert Ashton <r.ashton@imperial.ac.uk>
2019-10-30 12:26:34 +0000

    Avoid INLA building from dynamic libraries which don't exist

--- tests/testthat/helper-model.R
+++ tests/testthat/helper-model.R
@@ -3,7 +3,7 @@ Sys.setenv("USE_MOCK_MODEL" = "true")
 
 ## Don't use dynamic libaries as these don't currently work on travis with
 ## up to date INLA build
-##INLA:::inla.dynload.workaround()
+INLA:::inla.dynload.workaround()
 
 mock_model <- list(output_path = file.path("testdata", "malawi_output.RDS"),
                    spectrum_path = file.path("testdata", "malawi_spectrum_download.zip"),
