81208ef3d6c1a24f97b1da22e82e675e87b60276
Robert Ashton <r.ashton@imperial.ac.uk>
2019-11-13 14:56:22 +0000

    Fix version checking

--- R/version.R
+++ R/version.R
@@ -1,3 +1,7 @@
 is_current_version <- function(version) {
-  isTRUE(all.equal(cfg$version_info, version))
+  check_version <- function(component) {
+    cfg$version_info[[component]] == version[[component]]
+  }
+  matching_versions <- vapply(names(version), check_version, logical(1))
+  all(matching_versions) && length(version) == length(cfg$version_info)
 }
