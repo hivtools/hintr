d10ec72a5e724477ebde0d91735478936248055a
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-11-28 09:54:51 +0000

    Two more missing bits

--- tests/testthat/test-server.R
+++ tests/testthat/test-server.R
@@ -469,7 +469,7 @@ test_that("model run options are exposed", {
     "2018")
 
   expect_true(!is.null(response$version))
-  expect_equal(names(response$version), c("hintr", "naomi", "rrq"))
+  expect_equal(names(response$version), c("hintr", "naomi", "rrq", "traduire"))
   expect_true(all(grepl("^(\\d+\\.)?(\\d+\\.)?(\\*|\\d+)$", response$version)))
   })
 
@@ -497,7 +497,7 @@ test_that("version information is returned", {
   response <- response_from_json(r)
   expect_equal(response$status, "success")
   expect_setequal(names(response$data),
-                  c("hintr", "naomi", "rrq"))
+                  c("hintr", "naomi", "rrq", "traduire"))
 })
 
 test_that("worker information is returned", {
