d2aceaf21817cb5ad8c3f3d142e6d5b690df120e
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-12-03 16:05:49 +0000

    Add init test

--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -8,3 +8,14 @@ test_that("version info is loaded on package load", {
   version_info <- get_version_info()
   expect_equal(version_info, cfg$version_info)
 })
+
+test_that("translation initialisation calls traduire_register", {
+  expect_true("package:hintr" %in% traduire::translator_list())
+  expect_error(traduire::translator("package:hintr"), NA)
+  traduire::translator_unregister("package:hintr")
+  expect_false("package:hintr" %in% traduire::translator_list())
+  expect_error(traduire::translator("package:hintr"))
+  hintr_init_traduire()
+  expect_error(traduire::translator("package:hintr"), NA)
+  expect_true("package:hintr" %in% traduire::translator_list())
+})
