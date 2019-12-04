99c2019bf81abf3e3300c1a32b827f1cadd30e5d
Robert Ashton <r.ashton@imperial.ac.uk>
2019-12-04 18:16:22 +0000

    Fix test failuresg

--- tests/testthat/test-validation-asserts.R
+++ tests/testthat/test-validation-asserts.R
@@ -258,7 +258,7 @@ test_that("can check that a column contains only positive numeric values", {
   data <- data_frame(population=c(1,2,3), current_art=c("none",0,5), test_pos=c(-1,2,4))
 
   expect_true(assert_column_positive_numeric(data, "population"))
-  expect_error(assert_numeric_column(data, "current_art"),
+  expect_error(assert_column_positive_numeric(data, "current_art"),
                "Column current_art is required to be numeric. Non-numeric values were found.")
   expect_error(assert_column_positive_numeric(data, "test_pos"),
                "Column test_pos requires positive numeric values. Negative numeric values were found.")
