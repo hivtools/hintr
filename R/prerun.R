5ef609e14d61b833e72ca772259a87b900fe1fdd
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-12-04 10:17:52 +0000

    Add push support for prerun results

--- R/prerun.R
+++ R/prerun.R
@@ -79,6 +79,24 @@ prerun_import <- function(prerun, path, output = "output.rds",
   PrerunModelResults$new(prerun)$import(path, output, spectrum, summary)
 }
 
+##' Push prerun model results to naomi.dide.ic.ac.uk
+##'
+##' @title Push prerun model results to naomi.dide.ic.ac.uk
+##' @inheritParams prerun_import
+##' @export
+prerun_push <- function(path, output = "output.rds",
+                        spectrum = "spectrum.zip", summary = "summary.zip") {
+  loadNamespace("ssh")
+  session <- ssh::ssh_connect("incoming@naomi.dide.ic.ac.uk")
+  on.exit(ssh::ssh_disconnect(session))
+  id <- ids::random_id(1, 6)
+  dest <- sprintf("incoming/%s", id)
+  args <- c("hintr_prerun_import", dest, "--output", output,
+            "--spectrum", spectrum, "--summary", summary)
+  command <- paste(args, collapse = " ")
+  ssh::scp_upload(session, path, dest)
+  ssh::ssh_exec_wait(session, command)
+}
 
 read_info_inputs <- function(path) {
   tmp <- tempfile()
