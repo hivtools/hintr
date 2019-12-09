b622ce2d573b883a270f2911ad59132621a3064a
Robert Ashton <r.ashton@imperial.ac.uk>
2019-12-09 15:04:17 +0000

    Update naomi version and fix tests

--- tests/testthat/test-run-model.R
+++ tests/testthat/test-run-model.R
@@ -24,9 +24,10 @@ test_that("model can be run and filters extracted", {
   expect_length(barchart$filters[[2]]$options, 2)
   expect_equal(barchart$filters[[2]]$options[[1]]$id, scalar("CY2018Q3"))
   expect_equal(barchart$filters[[2]]$options[[1]]$label, scalar("Jul-Sep 2018"))
-  expect_equal(nrow(barchart$indicators), 8)
+  expect_equal(nrow(barchart$indicators), 10)
   expect_true(all(c("prevalence", "art_coverage", "current_art", "population",
-                    "plhiv", "incidence", "new_infections", "receiving_art") %in%
+                    "plhiv", "incidence", "new_infections", "receiving_art",
+                    "anc_prevalence", "anc_art_coverage") %in%
                     barchart$indicators$indicator))
 
   choropleth <- model_run$plottingMetadata$choropleth
@@ -45,9 +46,10 @@ test_that("model can be run and filters extracted", {
   expect_equal(choropleth$filters[[2]]$options[[1]]$id, scalar("CY2018Q3"))
   expect_equal(choropleth$filters[[2]]$options[[1]]$label,
                scalar("Jul-Sep 2018"))
-  expect_equal(nrow(choropleth$indicators), 8)
+  expect_equal(nrow(choropleth$indicators), 10)
   expect_true(all(c("prevalence", "art_coverage", "current_art", "population",
-                    "plhiv", "incidence", "new_infections", "receiving_art") %in%
+                    "plhiv", "incidence", "new_infections", "receiving_art",
+                    "anc_prevalence", "anc_art_coverage") %in%
                     choropleth$indicators$indicator))
 })
 
@@ -80,9 +82,10 @@ test_that("model without national level results can be processed", {
   expect_length(barchart$filters[[2]]$options, 2)
   expect_equal(barchart$filters[[2]]$options[[1]]$id, scalar("CY2018Q3"))
   expect_equal(barchart$filters[[2]]$options[[1]]$label, scalar("Jul-Sep 2018"))
-  expect_equal(nrow(barchart$indicators), 8)
+  expect_equal(nrow(barchart$indicators), 10)
   expect_true(all(c("prevalence", "art_coverage", "current_art", "population",
-                    "plhiv", "incidence", "new_infections", "receiving_art") %in%
+                    "plhiv", "incidence", "new_infections", "receiving_art",
+                    "anc_prevalence", "anc_art_coverage") %in%
                     barchart$indicators$indicator))
 
   choropleth <- model_run$plottingMetadata$choropleth
@@ -101,9 +104,10 @@ test_that("model without national level results can be processed", {
   expect_equal(choropleth$filters[[2]]$options[[1]]$id, scalar("CY2018Q3"))
   expect_equal(choropleth$filters[[2]]$options[[1]]$label,
                scalar("Jul-Sep 2018"))
-  expect_equal(nrow(choropleth$indicators), 8)
+  expect_equal(nrow(choropleth$indicators), 10)
   expect_true(all(c("prevalence", "art_coverage", "current_art", "population",
-                    "plhiv", "incidence", "new_infections", "receiving_art") %in%
+                    "plhiv", "incidence", "new_infections", "receiving_art",
+                    "anc_prevalence", "anc_art_coverage") %in%
                     choropleth$indicators$indicator))
 
 })
