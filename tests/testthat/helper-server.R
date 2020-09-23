990bfc40e5eca1c3ac0c6fc1e42f208d908b82e6
Robert Ashton <r.ashton@imperial.ac.uk>
2020-09-23 16:41:35 +0100

    Turn off swagger docs using updated syntax and increase poll wait time

--- tests/testthat/helper-server.R
+++ tests/testthat/helper-server.R
@@ -41,7 +41,7 @@ get_free_port <- free_port(9000)
 # up. So the default is to use an incrementing port number. This has
 # proven more reliable in practice anyway. The approach above is the
 # same used in vaultr.
-hintr_server <- function(n_tries = 10, poll = 0.5, results_dir = tempdir()) {
+hintr_server <- function(n_tries = 10, poll = 1, results_dir = tempdir()) {
   test_redis_available()
   skip_if_not_installed("callr")
   skip_if_not_installed("httr")
