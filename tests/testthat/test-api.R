59d11102b734c4d0be2facca6176f35201bc175e
Robert Ashton <r.ashton@imperial.ac.uk>
2020-10-01 14:12:55 +0100

    Remove garbage collects

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -292,9 +292,6 @@ test_that("endpoint_model_options", {
   expect_true(all(grepl("^(\\d+\\.)?(\\d+\\.)?(\\*|\\d+)$", body$version)))
 })
 
-## Add garbage collects to avoid intermittent failures
-gc()
-
 test_that("endpoint_model_options works", {
   test_redis_available()
   queue <- test_queue(workers = 0)
@@ -424,12 +421,9 @@ test_that("api can call endpoint_model_options_validate", {
   expect_true(body$data$valid)
 })
 
-## Add garbage collects to avoid intermittent failures
-gc()
-
 test_that("endpoint_model_submit can be run", {
   test_redis_available()
-  queue <- test_queue()
+  queue <- test_queue(workers = 1)
   endpoint <- endpoint_model_submit(queue)
   path <- setup_submit_payload()
   response <- endpoint$run(readLines(path))
@@ -723,9 +717,6 @@ test_that("returning_json_version adds version", {
                unclass(cfg$version_info$traduire))
 })
 
-## Add garbage collects to avoid intermittent failures
-gc()
-
 test_that("endpoint_download_spectrum can be run", {
   test_redis_available()
   test_mock_model_available()
@@ -998,9 +989,6 @@ test_that("api can call endpoint_model_debug", {
   ## Download contains data
   expect_true(length(res$body) > 1000000)
 })
-
-## Add garbage collects to avoid intermittent failures
-gc()
 
 test_that("endpoint_hintr_version works", {
   endpoint <- endpoint_hintr_version()
