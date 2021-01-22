c1a89e82c60a958c91445c967e1f04db14f68d15
Robert Ashton <r.ashton@imperial.ac.uk>
2021-01-22 17:27:26 +0000

    Save calibrate outputs to new files

--- R/calibrate.R
+++ R/calibrate.R
@@ -1,4 +1,5 @@
-run_calibrate <- function(model_output, calibration_options, language = NULL) {
+run_calibrate <- function(model_output, calibration_options, path_results,
+                          language = NULL) {
   if (!is.null(language)) {
     reset_hintr <- traduire::translator_set_language(language)
     reset_naomi <-
@@ -9,5 +10,14 @@ run_calibrate <- function(model_output, calibration_options, language = NULL) {
     })
   }
 
-  naomi::hintr_calibrate(model_output, calibration_options)
+  path_results <- normalizePath(path_results, mustWork = TRUE)
+  output_path <- tempfile(tmpdir = path_results, fileext = ".rds")
+  spectrum_path <- tempfile(tmpdir = path_results, fileext = ".zip")
+  coarse_output_path <- tempfile(tmpdir = path_results, fileext = ".zip")
+  summary_report_path <- tempfile(tmpdir = path_results, fileext = ".html")
+  calibration_path <- tempfile(tmpdir = path_results, fileext = ".rds")
+
+  naomi::hintr_calibrate(model_output, calibration_options, output_path,
+                         spectrum_path, coarse_output_path,
+                         summary_report_path, calibration_path)
 }
