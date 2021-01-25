3da53c70d140903f011210f7a03ece5e656c685f
Robert Ashton <r.ashton@imperial.ac.uk>
2021-01-25 11:09:09 +0000

    Run model & calibration in a separate process

--- R/run_model.R
+++ R/run_model.R
@@ -80,7 +80,7 @@ run_model <- function(data, options, path_results, path_prerun = NULL,
 
   naomi::hintr_run_model(data, options, output_path, spectrum_path,
                          coarse_output_path, summary_report_path,
-                         calibration_path, validate)
+                         calibration_path, validate = FALSE)
 }
 
 select_data <- function(data) {
