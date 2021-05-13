84ec0e81cf3af6f659cb5de7ab7bfaba81e09f2e
Robert Ashton <r.ashton@imperial.ac.uk>
2021-05-13 11:01:36 +0100

    Remove deprecated calibrate endpoint

--- R/endpoints.R
+++ R/endpoints.R
@@ -202,19 +202,6 @@ verify_result_available <- function(queue, id) {
   }
 }
 
-model_calibrate <- function(queue) {
-  function(id, input) {
-    verify_result_available(queue, id)
-    calibration_options <- jsonlite::fromJSON(input)
-    if (!is_current_version(calibration_options$version)) {
-      hintr_error(t_("MODEL_SUBMIT_OLD"), "VERSION_OUT_OF_DATE")
-    }
-    ## TODO: run calibration asynchronously, takes too long to do sync
-    ## see mrc-2040
-    process_result(queue$result(id))
-  }
-}
-
 model_cancel <- function(queue) {
   function(id) {
     tryCatch({
