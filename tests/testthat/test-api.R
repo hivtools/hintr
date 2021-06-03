e1c7dea447fc0dda5ae9085b1b43920725d8e37b
Robert Ashton <r.ashton@imperial.ac.uk>
2021-06-03 12:10:07 +0100

    Switch out status wait for task wait to make test more robust

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -604,8 +604,7 @@ test_that("erroring model run returns useful messages", {
   expect_equal(body$status, "success")
   expect_true(!is.null(body$data$id))
 
-  wait_status(body$data$id, queue$queue)
-
+  out <- queue$queue$task_wait(body$data$id)
   mock_id <- mockery::mock(scalar("fake_key"), cycle = TRUE)
   with_mock("ids::proquint" = mock_id, {
     res <- api$request("GET", sprintf("/model/result/%s", body$data$id))
