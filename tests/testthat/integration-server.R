f00a6730e34afa838d12945e0056440246d02827
Robert Ashton <r.ashton@imperial.ac.uk>
2021-07-29 12:02:02 +0100

    Fix integration tests

--- tests/testthat/integration-server.R
+++ tests/testthat/integration-server.R
@@ -764,9 +764,9 @@ test_that("input time series can return plot data for programme", {
   response <- response_from_json(r)
   expect_equal(response$status, "success")
   expect_equal(response$errors, NULL)
-  expect_equal(names(response$data), c("data", "filters", "defaults"))
+  expect_equal(names(response$data), c("data", "metadata"))
   expect_true(length(response$data$data) > 100)
-  expect_equal(names(response$data$defaults$selected_filter_options),
+  expect_equal(names(response$data$metadata$defaults$selected_filter_options),
                c("plot_type", "area_level", "time_step"))
 })
 
@@ -785,8 +785,8 @@ test_that("input time series can return plot data for anc", {
   response <- response_from_json(r)
   expect_equal(response$status, "success")
   expect_equal(response$errors, NULL)
-  expect_equal(names(response$data), c("data", "filters", "defaults"))
+  expect_equal(names(response$data), c("data", "metadata"))
   expect_true(length(response$data$data) > 100)
-  expect_equal(names(response$data$defaults$selected_filter_options),
+  expect_equal(names(response$data$metadata$defaults$selected_filter_options),
                c("plot_type", "area_level", "age"))
 })
