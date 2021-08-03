3ff292f408a558b863dcf99a7792a212a8db0626
Robert Ashton <r.ashton@imperial.ac.uk>
2021-08-03 16:50:52 +0100

    Use functions from naomi for shaping input time series data

--- R/time_series.R
+++ R/time_series.R
@@ -1,5 +1,5 @@
 get_programme_time_series <- function(programme, shape) {
-  data <- read_csv(system_file("extdata/dummy_input_time_series_programme.csv"))
+  data <- naomi::prepare_input_time_series_art(programme, shape)
   filters <- get_programme_time_series_filters(data)
   area_level_options <- get_selected_filter_options(filters, "area_level")
   list(
@@ -18,9 +18,7 @@ get_programme_time_series <- function(programme, shape) {
 }
 
 get_anc_time_series <- function(anc, shape) {
-  data <- read_csv(system_file("extdata/dummy_input_time_series_anc.csv"))
-  ## Force time period to a string - will be rendered as numeric otherwise
-  data$time_period <- as.character(data$time_period)
+  data <- naomi::prepare_input_time_series_anc(anc, shape)
   filters <- get_anc_time_series_filters(data)
   area_level_options <- get_selected_filter_options(filters, "area_level")
   list(
