4454426625b50a120128a01480b027cd62d06c33
rtesra <70330391+rtesra@users.noreply.github.com>
2022-01-11 14:30:41 +0000

    update anc time series test

--- tests/testthat/test-time-series.R
+++ tests/testthat/test-time-series.R
@@ -73,7 +73,7 @@ test_that("get_anc_time_series returns data and columns", {
   expect_equal(columns[[1]]$id, scalar("plot_type"))
   expect_equal(columns[[1]]$column_id, scalar("plot"))
   expect_equal(columns[[1]]$label, scalar("Plot type"))
-  expect_length(columns[[1]]$values, 4)
+  expect_length(columns[[1]]$values, 6)
   expect_setequal(names(columns[[1]]$values[[1]]),
                   c("id", "label", "description", "format", "accuracy"))
   expect_equal(columns[[2]]$id, scalar("area_level"))
