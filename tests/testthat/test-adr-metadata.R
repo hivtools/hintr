5a11850ade1bf7a971fe9a73f88a680ff60865a4
Robert Ashton <r.ashton@imperial.ac.uk>
2022-05-03 14:03:05 +0100

    Add comparison report to ADR metadata endpoint

--- tests/testthat/test-adr-metadata.R
+++ tests/testthat/test-adr-metadata.R
@@ -90,6 +90,36 @@ test_that("can return upload metadata for ADR summary report", {
   expect_s3_class(res$data$description, c("scalar", "character"))
 })
 
+test_that("can return upload metadata for comparison report", {
+  test_mock_model_available()
+  q <- test_queue_result()
+
+  ## Submit download request
+  submit <- endpoint_download_submit(q$queue)
+  submit_response <- submit$run(q$calibrate_id, "comparison")
+
+  expect_equal(submit_response$status_code, 200)
+  expect_true(!is.null(submit_response$data$id))
+
+  ## Status
+  out <- q$queue$queue$task_wait(submit_response$data$id)
+  status <- endpoint_download_status(q$queue)
+  status_response <- status$run(submit_response$data$id)
+
+  expect_equal(status_response$status_code, 200)
+  expect_equal(status_response$data$id, submit_response$data$id)
+  expect_true(status_response$data$done)
+  expect_equal(status_response$data$status, scalar("COMPLETE"))
+
+  ## Get metadata
+  metadata <- endpoint_adr_metadata(q$queue)
+  res <- metadata$run(submit_response$data$id)
+  expect_equal(res$status_code, 200)
+  expect_equal(names(res$data), c("type", "description"))
+  expect_equal(res$data$type, scalar("comparison"))
+  expect_s3_class(res$data$description, c("scalar", "character"))
+})
+
 test_that("trying to get ADR metadata without result available throws error", {
   test_mock_model_available()
   q <- test_queue_result()
