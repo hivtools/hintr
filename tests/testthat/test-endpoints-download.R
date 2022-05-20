ee58f7466b1559f65f50c6b4c5bc29c0cdb81ade
Robert Ashton <r.ashton@imperial.ac.uk>
2022-05-20 11:06:38 +0100

    Use more iso like date format, add test checks input validation through API

--- tests/testthat/test-endpoints-download.R
+++ tests/testthat/test-endpoints-download.R
@@ -501,17 +501,73 @@ test_that("spectrum download can include notes", {
     "Project notes:",
     "",
     "My project 123",
-    "May 17th 2022 12:34",
+    "2022/05/17 12:34:21",
     "These are my project notes",
     "",
     "Version notes:",
     "",
     "Version 2",
-    "May 17th 2022 12:34",
+    "2022/05/17 12:34:21",
     "Notes specific to this version",
     "",
     "Version 1",
-    "May 12th 2022 09:57",
+    "2022/05/14 09:12:54",
+    "Notes from the first version",
+    ""))
+})
+
+test_that("api: spectrum download can include notes", {
+  test_redis_available()
+  test_mock_model_available()
+  q <- test_queue_result()
+  api <- api_build(q$queue)
+
+  ## Submit download request
+  path <- setup_download_request_payload()
+  submit <- api$request("GET",
+                        paste0("/download/submit/spectrum/", q$calibrate_id),
+                        body = path)
+  submit_body <- jsonlite::fromJSON(submit$body)
+  expect_equal(submit$status, 200)
+  expect_true(!is.null(submit_body$data$id))
+
+  ## Status
+  out <- q$queue$queue$task_wait(submit_body$data$id)
+  status <- api$request("GET",
+                        paste0("/download/status/", submit_body$data$id))
+
+  expect_equal(status$status, 200)
+  status_body <- jsonlite::fromJSON(status$body)
+  expect_equal(status_body$data$id, submit_body$data$id)
+  expect_true(status_body$data$done)
+  expect_equal(status_body$data$status, "COMPLETE")
+
+  ## Get result
+  res <- api$request("GET", paste0("/download/result/", submit_body$data$id))
+
+  expect_equal(res$status, 200)
+
+  tmp <- tempfile()
+  dest <- tempfile()
+  writeBin(as.vector(res$body), tmp)
+  zip::unzip(tmp, exdir = dest)
+  expect_true("notes.txt" %in% list.files(dest))
+  notes <- readLines(file.path(dest, "notes.txt"))
+  expect_equal(notes, c(
+    "Project notes:",
+    "",
+    "My project 123",
+    "2022/05/17 12:34:21",
+    "These are my project notes",
+    "",
+    "Version notes:",
+    "",
+    "Version 2",
+    "2022/05/17 12:34:21",
+    "Notes specific to this version",
+    "",
+    "Version 1",
+    "2022/05/14 09:12:54",
     "Notes from the first version",
     ""))
 })
