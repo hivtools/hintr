f0b3abbb7fca8edbfe7654beff5f6818e9a59979
Mantra <mantrakusumgar@outlook.com>
2024-01-19 11:26:46 +0000

    add setup file

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,17 +1,4 @@
 library(testthat)
 library(hintr)
-library(mockery)
-
-call_with_mocks_object <- function(code, mocks) {
-    binded_code <- function(...) {
-        with_mocked_bindings(code, ...)
-    }
-    do.call(binded_code, mocks)
-}
-
-get_mock_args_from_vector <- function(vector) {
-  # mock args are lists of lists
-  lapply(as.list(vector), list)
-}
 
 test_check("hintr")
---
+++ tests/testthat/setup.R
@@ -0,0 +1,13 @@
+library(mockery)
+
+call_with_mocks_object <- function(code, mocks) {
+    binded_code <- function(...) {
+        with_mocked_bindings(code, ...)
+    }
+    do.call(binded_code, mocks)
+}
+
+get_mock_args_from_vector <- function(vector) {
+  # mock args are lists of lists
+  lapply(as.list(vector), list)
+}
