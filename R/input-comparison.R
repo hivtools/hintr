b33a91714ad0b6161ea8587885c3a6ec2ad4d8d3
Rob Ashton <robertashton94@gmail.com>
2024-11-11 16:52:57 +0000

    Hide filters users do not need to modify, add missing transalted labels

--- R/input-comparison.R
+++ R/input-comparison.R
@@ -30,12 +30,8 @@ input_comparison <- function(input) {
   })
 }
 
-get_filter_options_translated <- function(data, key, values = NULL) {
-  to_translate <- unique(data[[key]])
-  if (!is.null(values)) {
-    to_translate <- to_translate[to_translate %in% values]
-  }
-  lapply(to_translate, function(value) {
+get_filter_options_translated <- function(values = NULL) {
+  lapply(values, function(value) {
     list(id = scalar(value),
          label = scalar(t_(toupper(value))))
   })
@@ -72,12 +68,18 @@ build_input_comparison_metadata <- function(data) {
     column_id = scalar("year"),
     options = get_year_filters(data, decreasing = FALSE)
   )
+  art_groups <- c("art_adult_both", "art_adult_females", "art_adult_males",
+                  "art_children")
+  anc_groups <- "anc_adult_female"
+  ordered_groups <- c(art_groups, anc_groups)
   group_ids <- unique(data$group)
+  ordered_group_ids <- ordered_groups[ordered_groups %in% group_ids]
   group_filter <- list(
     id = scalar("group"),
     column_id = scalar("group"),
-    options = get_filter_options(data, "group")
+    options = get_filter_options_translated(ordered_group_ids)
   )
+
   list(
     filterTypes = list(
       indicator_filter,
@@ -92,7 +94,9 @@ build_input_comparison_metadata <- function(data) {
         group_ids,
         indicator_id_label_map),
       inputComparisonTable = get_input_table_settings(indicator_id_label_map,
-                                                      group_ids)
+                                                      group_ids,
+                                                      art_groups,
+                                                      anc_groups)
     )
   )
 }
@@ -184,13 +188,11 @@ get_input_barchart_settings <- function(indicator_ids,
   )
 }
 
-get_input_table_settings <- function(indicator_id_label_map, group_ids) {
+get_input_table_settings <- function(indicator_id_label_map, group_ids,
+                                     art_groups, anc_groups) {
   art_indicators <- "number_on_art"
-  art_groups <- c("art_adult_both", "art_adult_females", "art_adult_males",
-                  "art_children")
   anc_indicators <- c("anc_already_art", "anc_clients", "anc_known_pos",
                       "anc_tested", "anc_tested_pos")
-  anc_groups <- "anc_adult_female"
   indicator_to_control_option <- function(indicator_id) {
     if (indicator_id %in% art_indicators) {
       groups <- art_groups[art_groups %in% group_ids]
@@ -218,6 +220,7 @@ get_input_table_settings <- function(indicator_id_label_map, group_ids) {
   list(
     defaultEffect = list(
       setFilters = default_set_filters,
+      setHidden = c("indicator", "year", "group"),
       customPlotEffect = list(
         row = c("area_name", "year"),
         column = "group"
@@ -229,50 +232,6 @@ get_input_table_settings <- function(indicator_id_label_map, group_ids) {
         id = scalar("indicator_control"),
         label = scalar(t_("OUTPUT_FILTER_INDICATOR")),
         options = indicator_control_options
-      )
-    )
-  )
-}
-
-get_table_presets <- function(filter_types) {
-  detail_options <- get_filter_option_ids(filter_types, "detail")
-  list(
-    list(
-      id = scalar("sex_by_area"),
-      label = scalar(t_("TABLE_SEX_BY_AREA")),
-      effect = list(
-        setFilters = lapply(
-          c("indicator", "area", "detail", "period", "sex", "age"),
-          get_filter_from_id),
-        ## Hide the area ID as we want people to just select the detail level
-        ## and see all rows within that level. Having the area filter available
-        ## too makes this confusing, but we need area to exist for picking
-        ## up the row labels
-        setHidden = c("area"),
-        setMultiple = c("sex", "area"),
-        setFilterValues = list(
-          detail = detail_options[length(detail_options)]
-        ),
-        customPlotEffect = list(
-          row = c("area"),
-          column = c("sex")
-        )
-      )
-    ),
-    list(
-      id = scalar("sex_by_5_year_age_group"),
-      label = scalar(t_("TABLE_SEX_BY_5_YEAR_AGE_GROUP")),
-      effect = list(
-        setFilters = lapply(c("indicator", "area", "period", "sex", "age"),
-                            get_filter_from_id),
-        setMultiple = c("sex", "age"),
-        setFilterValues = list(
-          age = naomi::get_five_year_age_groups()
-        ),
-        customPlotEffect = list(
-          row = c("age"),
-          column = c("sex")
-        )
       )
     )
   )
